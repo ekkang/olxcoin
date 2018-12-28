61d38e2cdf28e00256571cb12f5abb8965c88b8d
ekkang <ekkang0713@nate.com>
2018-12-28 11:37:20 +0900

    임시 수정중

--- src/main.cpp
+++ src/main.cpp
@@ -2849,7 +2849,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x09a4eb5c97e1a0a7abe7e971151d57da24cd08afc3e1b54ea826bff48520b229"));
+        assert(block.hashMerkleRoot == uint256("0xb464be30f1c73abd5723fa487bb7496cf39dbbdabce24619a9ac1a6df6d6e496"));
         block.print();
         assert(hash == hashGenesisBlock);
 
